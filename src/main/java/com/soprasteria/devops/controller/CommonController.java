1022fcfe89fbd5d1dd1f802fc919ef839af25289
edison009 <anandedison@gmail.com>
2022-10-03 10:22:59 +0530

    Add files via upload

---
+++ src/main/java/com/soprasteria/devops/controller/CommonController.java
@@ -0,0 +1,27 @@
+package com.soprasteria.devops.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.soprasteria.devops.model.BaseObj;
+
+@RestController
+public class CommonController {
+	
+	@GetMapping(value="/hello-world")
+	public BaseObj helloWorld() {
+		
+		return new BaseObj("2034", "DEVOPS Accelerator", "Yozy Technologies LLP");
+	}
+	@GetMapping(value="/hello-world/yozyme")
+	public BaseObj Aravind() {
+		
+		return new BaseObj("2033", "DEVOPS Accelerator", "Yozy Technologies LLP");
+	}
+	@GetMapping(value="/yozyme")
+	public BaseObj Kumar() {
+		
+		return new BaseObj("2031", "DEVOPS Accelerator", "Yozy Technologies");
+	}
+
+}
